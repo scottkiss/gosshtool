06696ddbee81f7badd190b936f6c5a178849a623
scottkiss <skkmvp@hotmail.com>
2016-03-08 15:49:48 +0800

    support multi client execute command

--- tool.go
+++ tool.go
@@ -1,12 +1,13 @@
 package gosshtool
 
 import (
+	"errors"
 	"sync"
 )
 
 var (
 	sshClients      map[string]*SSHClient
-	sshClientsMutex sync.Mutex
+	sshClientsMutex sync.RWMutex
 )
 
 func init() {
@@ -14,12 +15,12 @@ func init() {
 }
 
 func NewSSHClient(config *SSHClientConfig) (client *SSHClient) {
-	sshClientsMutex.Lock()
+	sshClientsMutex.RLock()
 	client = sshClients[config.Host]
 	if client != nil {
 		return
 	}
-	sshClientsMutex.Unlock()
+	sshClientsMutex.RUnlock()
 	client = new(SSHClient)
 	client.Host = config.Host
 	client.User = config.User
@@ -30,4 +31,25 @@ func NewSSHClient(config *SSHClientConfig) (client *SSHClient) {
 	sshClients[config.Host] = client
 	sshClientsMutex.Unlock()
 	return client
+}
+
+func getClient(hostname string) (client *SSHClient, err error) {
+	if hostname == "" {
+		return nil, errors.New("host name is empty")
+	}
+	sshClientsMutex.RLock()
+	client = sshClients[hostname]
+	if client != nil {
+		return client, nil
+	}
+	sshClientsMutex.RUnlock()
+	return nil, errors.New("client not create")
+}
+
+func ExecuteCmd(cmd, hostname string) (output, errput string, err error) {
+	client, err := getClient(hostname)
+	if err != nil {
+		return
+	}
+	return client.Cmd(cmd)
 }
